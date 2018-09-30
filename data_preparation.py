3520a2167d59e04d15fc4866ed90bc82907cfbd6
kacper <kacper.zyla97@gmail.com>
2018-09-30 17:51:48 +0200

    clining data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -1,7 +1,5 @@
 import csv
 import numpy as np
-from matplotlib.pylab import figure, plot, subplot, xlabel, ylabel, hist, show
-from scipy import stats
 
 datasetpath = "../Bike-Sharing-Dataset"
 
@@ -16,18 +14,7 @@ for row in daydata:
     X.append([row[0]] + row[2:])
 
 X = np.array(X).astype(float)
-print(attribute_names)
 
-print((np.median(X[:, 11])))
-print((np.std(X[:, 11])))
-print(np.mean(X[:,11]))
-
-
-# figure()
-#
-# plot(X[:10,9],X[:10,15],'.')
-#
-# show()
 
 
 
