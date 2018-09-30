27c95f1334b4b12700ab3bbc4818e66d4879963e
kacper <kacper.zyla97@gmail.com>
2018-09-30 19:41:29 +0200

    deleting indices from X

--- data_preparation.py
+++ data_preparation.py
@@ -8,10 +8,10 @@ daydata = csv.reader(file, delimiter=',')
 
 row1 = next(daydata)
 
-attribute_names = [row1[0]] + row1[2:]
+attribute_names = row1[2:]
 X = []
 for row in daydata:
-    X.append([row[0]] + row[2:])
+    X.append(row[2:])
 
 X = np.array(X).astype(float)
 
